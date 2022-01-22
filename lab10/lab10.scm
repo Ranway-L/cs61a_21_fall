85910fc10c510abb2088316309b16a4a135a579b
Ranway <lxw823247061@gmail.com>
2022-01-22 19:04:08 +0800

    lab10 required questions finished

---
+++ lab10/lab10.scm
@@ -0,0 +1,38 @@
+; (define (over-or-under num1 num2)
+;   'YOUR-CODE-HERE
+;   (if (< num1  num2)
+;       -1
+;       (if (= num1 num2)
+;           0
+;           1))
+; )
+(define (over-or-under num1 num2)
+  'YOUR-CODE-HERE
+  (cond 
+    ((< num1 num2) -1)
+    ((= num1 num2) 0)
+    ((> num1 num2) 1)))
+
+(define (make-adder num)
+  'YOUR-CODE-HERE
+  (lambda (inc) (+ num inc))
+)
+
+(define (composed f g)
+  'YOUR-CODE-HERE
+  (lambda (x) (f (g x)) )
+)
+
+(define lst
+  (cons (cons 1 nil) (cons 2 (cons (cons 3 (cons 4 nil))(cons 5 nil))))
+    ; (list
+    ;       (cons 1 nil) 
+    ;       2 
+    ;       (cons 3 (cons 4 nil))
+    ;       5)
+)
+
+(define (remove item lst)
+  'YOUR-CODE-HERE
+  
+)
